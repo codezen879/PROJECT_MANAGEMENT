f17b3002df569256f59360ce862bd954ee9a5b35
Sanchit Tripathi <95371909+codezen879@users.noreply.github.com>
2024-04-28 20:26:27 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,7 +24,6 @@ function App() {
     <Router>
       <Routes>
       <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Login />} />
       <Route path="/register" element={<MProtected Component={Register} />} />
       <Route path="/ProjectSubmitted" element={<Protected Component={ProjectSubmitted} />} />
       <Route path="/employee" element={<Protected Component={Employee} />} />
