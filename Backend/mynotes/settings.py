63422d38f91c1703fdb2c083f13bb8eb0f0cf8ff
Sanchit Tripathi <95371909+codezen879@users.noreply.github.com>
2025-08-16 17:14:41 +0530

    Update settings.py

--- Backend/mynotes/settings.py
+++ Backend/mynotes/settings.py
@@ -90,7 +90,8 @@ DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
