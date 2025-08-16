4a1797d6a1fd795a5a7b79a10abbed9435802615
Sanchit Tripathi <95371909+codezen879@users.noreply.github.com>
2025-08-16 23:58:35 +0530

    Update settings.py

--- Backend/mynotes/settings.py
+++ Backend/mynotes/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'mynotes.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default="postgresql://pm_2425_user:gb4Fxbs4iOYSQ8cVGpt384Z31DLDnIFV@dpg-d2g6nb0gjchc73aosk60-a.oregon-postgres.render.com/pm_2425??sslmode=require",
+        default="postgresql://pm_2425_user:gb4Fxbs4iOYSQ8cVGpt384Z31DLDnIFV@dpg-d2g6nb0gjchc73aosk60-a.oregon-postgres.render.com/pm_2425?sslmode=require",
         conn_max_age=600,
         ssl_require=True
     )
